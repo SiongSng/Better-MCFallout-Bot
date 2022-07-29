3193e6d9f7f8e9a243369c98de471e674588dba1
SiongSng <rrt467778@gmail.com>
2022-07-29 15:54:41 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  window.loadFile(path.join(__dirname, "../index.html"));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/preload.ts
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
