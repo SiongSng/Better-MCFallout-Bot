5c75df0e3ad7d8584afb2fd0b767809644ead75b
菘菘 <rrt467778@gmail.com>
2022-08-02 22:39:45 +0800

    fix: run core on macOS (#2)

--- script/build_macos.sh
+++ script/build_macos.sh
@@ -4,9 +4,4 @@ cd app
 echo "[Info] Building the app"
 flutter build macos --release
 
-echo "[Info] Copying the core to the app"
-cd ..
-mkdir -p app/build/macos/Build/Products/Release/lib; mv core/out/better-mcfallout-bot $_/better-mcfallout-bot-core
-ls app/build/macos/Build/Products/Release/lib
-
 echo "[Info] Done"
