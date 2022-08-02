08d7aa36bf2be5967c177dba4248e356f73336c6
菘菘 <rrt467778@gmail.com>
2022-08-02 07:43:46 +0000

    try fix

--- script/build_macos.sh
+++ script/build_macos.sh
@@ -6,7 +6,7 @@ flutter build macos --release
 
 echo "[Info] Copying the core to the app"
 cd ..
-mkdir -p app/build/macos/Build/Products/Release/lib; mv core/out/better-mcfallout-bot $_/better-mcfallout-bot-core
-ls app/build/macos/Build/Products/Release/lib
+mkdir -p app/build/macos/Build/Products/Release/better_mcfallout_bot.app/Contents/MacOS/lib; mv core/out/better-mcfallout-bot $_/better-mcfallout-bot-core
+ls app/build/macos/Build/Products/Release/better_mcfallout_bot.app/Contents/MacOS/lib
 
 echo "[Info] Done"
